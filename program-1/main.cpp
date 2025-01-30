db9bcbd9a76091943688044f31915784e952db00
lavender-aa <sodaissoda@gmail.com>
2025-01-30 14:55:59 -0500

    work on program 1

--- program-1/main.cpp
+++ program-1/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
 #include "set.h"
-using std::cout, std::endl;
+using std::cout, std::endl, std::string;
+
+void print_menu();
+int get_menu_choice();
 
 int main()
 {
-    
+    bool quit = false;
+
+    while(!quit) {
+        print_menu();
+        int choice = get_menu_choice();
+    }
 }
+
+void print_menu() {
+    cout << "Options:" << endl
+         << "--------" << endl
+         << "\t[i]: insert a new integer into the set" << endl
+         << "\t[s]: search for an integer in the set (sets current number)" << endl
+         << "\t[r]: remove current number from the set" << endl
+         << "\t[d]: display the list" << endl
+         << "\t[q]: quit the program" << endl;
+}
+
