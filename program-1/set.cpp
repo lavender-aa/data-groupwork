8bc24bbace51b38b409093d9894aca3a94976b3b
IsaacDerg <tannerlauritzen@gmail.com>
2025-01-30 10:53:42 -0500

    added remove_current functionality
    added and commented remove_current function code

--- program-1/set.cpp
+++ program-1/set.cpp
@@ -26,7 +26,24 @@ void Set::insert(int)
 
 int Set::remove_current()
 {
+    // variables
+    Node* temp = current->next;
+    int val = current->data;
 
+    // get data from next node
+    current->data = current->next->data;
+    
+    // set pointer
+    current->next = temp->next;
+
+    // delete the node
+    delete temp;
+
+    // set current back to head
+    current = head;
+
+    // return value removed
+    return val;
 }
 
 
