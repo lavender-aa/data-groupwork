2b241ec7b3991556ebb1460e3ac6f2d98cbefc77
lavender-aa <sodaissoda@gmail.com>
2025-01-31 17:16:02 -0500

    fixed set printing bug, added spacing and label for printing

--- program-1/set.cpp
+++ program-1/set.cpp
@@ -33,11 +33,17 @@ bool Set::search(int)
 
 std::ostream& operator<< (std::ostream& stream, Set set)
 {
+    stream << "Set elements:\n";
+
     // for each node in the list, print out the number in its own line
     Node* temp = set.head;
     while(temp != nullptr) {
-        stream << "data: " << temp->data << std::endl;
+        stream << "data: " << temp->data << "\n";
+        temp = temp->next;
     }
+
+    // spacing
+    stream << "\n";
     
     return stream;
 }
