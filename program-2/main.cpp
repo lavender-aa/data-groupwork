532f513a90fafeb8512b58f0566bbf703e22144f
IsaacDerg <tannerlauritzen@gmail.com>
2025-03-07 21:36:59 -0500

    removed tests

--- program-2/main.cpp
+++ program-2/main.cpp
@@ -32,97 +32,15 @@ all testing stuff below main() should be removed once the pr is approved (before
 */
 
 // prototypes
-// TODO: remove tests
-void run_tests();
-void test1();
-void test2();
-void test3();
 
 
 int main()
 {
-    run_tests();
+
+
+    return 0;
 }
 
 
 
-// testing methods
-// TODO: remove
-#include <cassert>
-#include <cstring>
-
-void run_tests() {
-    test1();
-    test2();
-    test3();
-}
-
-// test popping, viewing empty stack
-void test1() {
-    cout << "test 1: pop and view empty stack\n";
-
-    Stack stack = Stack();
-
-    try {
-        stack.pop();
-    }
-    catch (std::underflow_error err) {
-        assert((std::string)err.what() == "Attempted to pop empty stack.");
-    }
-
-    try {
-        cout << stack.view_top() << "\n";
-    }
-    catch (std::underflow_error err) {
-        assert((std::string)err.what() == "Attempted to view top of empty stack.");
-    }
-
-    cout << "test 1 passed.\n\n";
-}
-
-// test filing stack, view top, and popping
-void test2() {
-    cout << "test 2: fill and empty stack\n";
-
-    Stack stack = Stack();
-
-    // ensure value returned from stack_top() matches value inserted
-    for(int i = 0; i < 5; i++) {
-        stack.push(i);
-        assert(stack.view_top() == i);
-    }
-
-    // ensure value returned from pop() matches value inserted
-    for(int i = 4; i >= 0; i--) {
-        int num = stack.pop();
-        assert(num == i);
-    }
-
-    cout << "test 2 passed.\n\n";
-}
-
-// test filling then emptying stack too far
-void test3() {
-    cout << "test 3: fill, empty stack too far\n";
-
-    Stack stack = Stack();
-
-    // fill stack with 0-9
-    for(int i = 0; i < 10; i++) {
-        stack.push(i);
-    }
-
-    // pop stack 11 times
-    for(int i = 0; i < 11; i++) {
-        try {
-            stack.pop();
-            assert(i != 10); // 11th pop should fail
-        }
-        catch (std::underflow_error err) {
-            assert((std::string)err.what() == "Attempted to pop empty stack.");
-            assert(i == 10);
-        }
-    }
-
-    cout << "test 3 passed.\n\n";
-}
+// functions
